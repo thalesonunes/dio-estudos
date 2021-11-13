a3f482b89b34178bf56fa220c95d096d354b0086
Thales Nunes <thales.o.nunes@gmail.com>
2021-11-13 13:46:03 -0300

    Finalizando o básico do Netflix Clone

---
+++ recriando-a-interface-do-netflix/js/owl/setup.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive: {
+        0: {
+            items: 2
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 4
+        },
+        1500: {
+            items: 7
+        }
+    }
+})
