9e44e1a57580ab46c2ed190f2f2d325fc858ba76
Thales Nunes <thales.o.nunes@gmail.com>
2021-11-15 15:52:31 -0300

    Exercícios realizados

---
+++ dio-javascript-estudos/02-variaveis-e-tipos/palindromo.js
@@ -0,0 +1,32 @@
+// Primeira solução
+// Invertendo a string existente e comparando o resultado com a string original
+
+function verificaPalindromo(string) {
+
+    if (!string) return "String inexistente!"; // se não for uma string já sai da função
+
+    console.log(string === string.split('').reverse().join(''));
+}
+verificaPalindromo('roma amor');
+
+
+// Segunda Solução
+// Dividindo a string ao meio e comparando as metades
+
+function verificaPalindromo2(string) {
+
+    if (!string) return "String inexistente!"; // se não for uma string já sai da função
+
+    if (!string.length) return "String vazia!"; // se o tamanho da string for vazio, também sai da função
+
+    for (var i = 0; i < string.length / 2; i++) {
+        if (string[i] !== string[string.length - 1 - i]) {
+            return console.log(false);
+        }
+    }
+
+    return console.log(true);
+}
+
+verificaPalindromo2('ovo');
+
---
+++ dio-javascript-estudos/02-variaveis-e-tipos/substituiPares.js
@@ -0,0 +1,23 @@
+function substituirNumerosPares(array) {
+
+    if (!array.length) return console.log("Array vazio");
+    if (!array) return console.log("Este array não existe!");
+
+    const naoZero = (num) => num !== 0;
+    const numPar = (num) => num % 2 === 0;
+
+    for (let i = 0; i < array.length; i++) {
+
+        if (numPar(array[i]) && naoZero(array[i])) {
+            console.log(`trocando ${array[i]} por 0...`);
+            array[i] = 0;
+
+        } else if (!naoZero(array[i])) {
+            console.log('Eita, você já vale 0!');
+        }
+    }
+    console.log(array);
+}
+
+substituirNumerosPares([2, 0, 3, 5, 22, 15, 3, 8]);
+substituirNumerosPares([]);
