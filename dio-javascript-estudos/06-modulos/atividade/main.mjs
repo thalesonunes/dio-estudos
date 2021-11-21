52775a18bb4d088ceb1d92d445df9b2dab629bc7
Thales Nunes <thales.o.nunes@gmail.com>
2021-11-21 09:42:53 -0300

    Exercício realizado!

---
+++ dio-javascript-estudos/06-modulos/atividade/main.mjs
@@ -0,0 +1,13 @@
+import {
+    mostraIdade
+} from "./funcoes.mjs";
+import {
+    mostraCidade
+} from "./funcoes.mjs";
+import {
+    mostraHobby
+} from "./funcoes.mjs";
+
+console.log(mostraIdade("Thales", 36));
+console.log(mostraCidade("Thales", "Passos/MG"));
+console.log(mostraHobby("Thales", "fotografia"));
