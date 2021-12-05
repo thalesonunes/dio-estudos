2634a391b4ace014d89f4eb5d379fde363864be7
Thales Nunes <thales.o.nunes@gmail.com>
2021-12-05 11:09:08 -0300

    Exercícios realizados

---
+++ dio-javascript-estudos/09-javascript-assincrono/projeto-html/assets/js/scripts.js
@@ -0,0 +1,25 @@
+const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
+
+const btn = document.getElementById('change-cat');
+
+const getCats = async () => {
+	try {
+		const data = await fetch(BASE_URL);
+		const json = await data.json();
+		return json.webpurl;
+	} catch (e) {
+		console.log(e.message);
+	}
+};
+
+const loadImg = async () => {
+	const img = document.getElementsByTagName('img')[0];
+	img.src = await getCats();
+};
+
+btn.addEventListener('click', loadImg);
+
+loadImg();
+
+
+
